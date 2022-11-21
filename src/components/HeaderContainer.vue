567bb05bdcb2db303a8f80cf8d2642ed4350b23d
Clara Moreira <anacacamm@gmail.com>
2022-11-21 12:31:17 -0300

    initial commit

---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        To-do list
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContainer'
+}
+</script>
+
+<style>
+header {
+  background: #42b983;
+  height: 5vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #e1e1e6;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+</style>
